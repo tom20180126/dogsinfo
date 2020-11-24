eb45d5e48188e7613faf5301914d9d75675b738b
tom20180126 <68907274+tom20180126@users.noreply.github.com>
2020-11-24 20:25:34 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'infomations/index'
   devise_for :users
+  root to: 'infomations#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
